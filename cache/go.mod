9f68d7cde233c34623f831e38e5dbd3a91fe23fb
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2026-08-07 05:50:33 +0600

    Delete cache directory

--- cache/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Abdullah-Builds/cache
-
-go 1.26.5
--- cache/internal/cache/item.go
+++
@@ -1,8 +0,0 @@
-package cache
-
-import "time"
-
-type Item struct {
-	Value     string    `json:"value"`
-	ExpiresAt time.Time `json:"expires_at"`
-}
